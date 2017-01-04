f22a21ad8fc1182ab601b332bb1142dc1c0d46b7
oucqiangz <605067184@qq.com>
2017-01-05 00:45:26 +0800

    feature1

---
+++ kaggle-code/House-Price/feature1.r
@@ -0,0 +1,8 @@
+# build more feature.
+# @author : oucqiangz (qiangz2012@yeah.net)
+
+# load cleaned source train data.
+cln_data <- read.csv( file = "G:\\Mypro\\GitHub\\kaggle-code\\House-Price\\train_clean.csv" )
+
+# analys category feature.
+
