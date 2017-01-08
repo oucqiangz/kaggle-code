a76670d831f0a4a33c0619742040f8c3e3bd5269
oucqiangz <605067184@qq.com>
2017-01-08 23:29:56 +0800

    category feature analysis

--- kaggle-code/House-Price/feature1.r
+++ kaggle-code/House-Price/feature1.r
@@ -2,26 +2,38 @@
 # @author : oucqiangz (qiangz2012@yeah.net)
 
 # load cleaned source train data.
-cln_data <- read.csv( file = "D:\\GitHub\\kaggle-code\\House-Price\\train_clean.csv" )
+cln_data <- read.csv( file = "G:\\Mypro\\GitHub\\kaggle-code\\House-Price\\train_clean.csv" )
 
-# category feature analysis.
-library(dplyr)
-#tbl_data <- tbl_df(cln_data)
-#grouped <- group_by(tbl_data,MSSubClass)
-#y.mean <- as.data.frame( summarise(grouped, mean(salePrices)) )
-#print(y.mean)
+##### category feature analysis.
 library(ggplot2)
-# category : MSSubClass
+## category : MSSubClass
 ggplot(cln_data, aes(x = MSSubClass, y = salePrices)) +
   # plot
   geom_point() +
   scale_x_continuous(breaks=seq(0, 200, 10))   ## 修改坐标轴的刻度间隔：X 轴每隔 10 个单位显示一个刻度
 
-# category : MSZoning
-ggplot(cln_data, aes(x = MSZoning, y = salePrices)) +
-  # 条形图函数：stat表明取用样本点对应纵轴值
-#  geom_bar(stat = "identity")
-  geom_col()
+## category : MSZoning,Street,LotShape,LandContour,Utilities,LotConfig,LandSlope,Neighborhood
+##            Condition1,Condition2,BldgType,HouseStyle,
+library(dplyr)
+
+# count.
+ggplot(cln_data, aes(x = MasVnrType)) +
+  geom_bar()   # y-axis：case count
+
+# mean.
+tbl_data <- tbl_df(cln_data)
+grouped <- group_by(tbl_data,MasVnrType)
+y.mean <- as.data.frame( summarise(grouped, mean(salePrices)) )
+colnames(y.mean) <- c("MasVnrType","yMean")
+
+ggplot(y.mean, aes(x = MasVnrType, y = yMean)) +
+  geom_col()   # y-axis：salePrice mean
+
+
+
+
+
+
 
 
 
